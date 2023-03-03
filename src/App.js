41c97af67387d790fabb01e233d7cf773b7352c7
bygabymora <bygabymora@gmail.com>
2023-03-02 20:37:03 -0500

    modified:   src/App.js

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { Box } from '@mui/material';
 import Home from './pages/Home';
+import Navbar from './components/Navbar';
 import Footer from './components/Footer';
-import Home from './pages/Home';
 
 const App = () => {
   return (
     <Box className='appBox'>
-        <Home/>
+        <Navbar/>
         <Routes>
             <Route path='/' element={<Home/>}/>
         </Routes>
