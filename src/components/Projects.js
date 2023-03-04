66991c18404539c29c06eb3d90d831db27778c5d
bygabymora <bygabymora@gmail.com>
2023-03-04 15:09:11 -0500

    modified:   src/App.css 	new file:   src/assets/images/gymapp.gif 	modified:   src/components/Projects.js

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,14 +1,20 @@
 import React from 'react'
 import { Stack } from '@mui/material';
+import gymApp from '../assets/images/gymapp.gif'
 
 const Projects = () => {
   return (
     <Stack className='about' id='projects'> 
-    <Stack>Projects done</Stack>
+    <Stack className='aboutMeTitle'>Projects done</Stack>
+    <br/> <br/>
     <Stack>
-      
+      <a href='https://bygabymora.github.io/gymapp/' target='_blank'>
+        <img src={gymApp} className='projectsDone'/>
+      </a>
+      <p> The Gym App </p>
+      <br/> <br/>
     </Stack>
-    <Stack>Projects in process</Stack>
+    <Stack className='aboutMeTitle'>Projects in process</Stack>
     </Stack>
   )
 }
