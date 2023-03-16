16df77a546070707bf57f440911082aea261921c
bygabymora <bygabymora@gmail.com>
2023-03-16 08:57:11 -0500

    modified:   src/App.css 	modified:   src/components/About.js 	modified:   src/components/Navbar.js

--- src/components/About.js
+++ src/components/About.js
@@ -6,6 +6,7 @@ import { useInView } from "react-intersection-observer";
 const About = () => {
 
   const [ref, inView] = useInView({ threshold: 0.01 })
+  
   return (
     <Stack className="section" direction={{ xs: 'column' , sm: 'column' , md: 'column' , lg: 'row' , xl: 'row'}} >
       <div>
@@ -20,7 +21,7 @@ const About = () => {
          and I've focused in modern apps using React.js with Hooks.<br/><br/>
 
         My passion for coding started years ago, 
-        but until a little time ago I became serious about it. 
+        and recently I became serious about it. 
         I'd love to participate in any project or an development 
         team that keeps me growing and learning.
         </p>
